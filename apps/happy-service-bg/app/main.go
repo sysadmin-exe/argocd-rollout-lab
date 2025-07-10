99511ad2dad05583e616fb8b5c405d572ee6890d
Victor Efedi <victorefedi@gmail.com>
2025-07-10 19:23:50 +0200

    Update Happy -logging

--- apps/happy-service-bg/app/main.go
+++ apps/happy-service-bg/app/main.go
@@ -18,10 +18,19 @@ func main() {
 	http.HandleFunc("/static", home)
 	http.HandleFunc("/ping", pingHandler) // Add the ping handler
 
+	loggedMux := loggingMiddleware(http.DefaultServeMux)
+
 	log.Printf("Starting server on %s...\n", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
+	if err := http.ListenAndServe(port, loggedMux); err != nil {
 		log.Fatalf("Error: %v\n", err)
 	}
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
